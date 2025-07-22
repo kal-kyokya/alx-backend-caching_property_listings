f0fabb403ae845e87a1fdca17ad3bff7732e5134
kal-kyokya <kalkyokya4@gmail.com>
2025-07-22 11:36:50 +0000

    Configure PostgresQL as the project's main database.

--- alx_backend_caching_property_listings/settings.py
+++ alx_backend_caching_property_listings/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +79,12 @@ WSGI_APPLICATION = 'alx_backend_caching_property_listings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME', 'alx_properties_db'),
+        'USER': os.environ.get('DATABASE_USER', 'alx_user'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'alx_password'),
+        'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
+        'PORT': os.environ.get('DATABASE_PORT','5432'),
     }
 }
 
