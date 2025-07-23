1f0d4f3d431aa614bae33eae0ec9510e312fe2bd
kal-kyokya <kalkyokya4@gmail.com>
2025-07-23 10:33:08 +0000

    Create a 'Django view' for the 'Property App' handling request for all stored property record.

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .models import Property
+import jsob
 
-# Create your views here.
+@cache_page(60 * 15)
+def property_list(request):
+    """Returns a list of all properties, with the response cached in Redis."""
+    properties = Property.objects.all()
+
+    data = []
+    for prop in properties:
+        data.append({
+            'id': prop.id,
+            'title': prop.title,
+            'description': prop.description,
+            'price': prop.price,
+            'location': prop.location,
+            'created_at': prop.created_at,
+        })
+
+    return JsonResponse(data, safe=False)
